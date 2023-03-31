f9c144c07541638eb916cb6c6bc65ba4d49ae3a1
Romane Orlando Robb <mail@romanerobb.com>
2023-03-31 13:32:36 -0400

    Updated project.

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0){
+        res.write("<h1>Yay, it's the weekend!</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+}
+});
+
+app.listen(3000, function(){
+    console.log("Robb Server started on port 3000");
+});
