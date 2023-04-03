d401e5b84ca8dcf3ee24c8ba7f96fd0974934fae
Romane Orlando Robb <mail@romanerobb.com>
2023-04-03 15:55:42 -0400

    Updated list.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -13,31 +16,36 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-
-  var day = today.toLocaleDateString("en-US", options);
+  let day = date();
 
   res.render("list", { 
-    kindOfDay: day,
-    newListItems: items
-  });
+    listTitle: day,
+    newListItems: items});
   
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
 
-  items.push(item);
-  
-  res.redirect("/");
-})
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+  workItems.push(item);
+  res.redirect("/work")
+  } else {
+    workItems.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+listTitle: "Work List",
+newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 app.listen(3000, function () {
   console.log("Robb Server started on port 3000");
